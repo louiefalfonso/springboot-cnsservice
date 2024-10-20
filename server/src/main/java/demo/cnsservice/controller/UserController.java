36b7458dfa293be0f0666dfca6174604e56ac66b
Louie Alfonso <lfriasalfonso@gmail.com>
2024-10-20 22:46:46 +0100

    Update Client Files

--- server/src/main/java/demo/cnsservice/controller/UserController.java
+++ server/src/main/java/demo/cnsservice/controller/UserController.java
@@ -2,11 +2,9 @@ package demo.cnsservice.controller;
 
 import demo.cnsservice.entity.User;
 import demo.cnsservice.service.UserService;
-import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
