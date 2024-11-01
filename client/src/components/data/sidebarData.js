33277f05f49b2476e51e437e9ba9a1168251fa7b
Louie Alfonso <lfriasalfonso@gmail.com>
2024-11-01 21:30:30 +0000

    Added Layout Components at Client

---
+++ client/src/components/data/sidebarData.js
@@ -0,0 +1,31 @@
+const sidebarData = [
+  {
+    id: "dashboard",
+    label: "Dashboard",
+    link: "/dashboard",
+  },
+  {
+    id: "amenities",
+    label: "Amenities",
+    link: "/amenities",
+  },
+  {
+    id: "Bookings",
+    label: "Bookings",
+    link: "/bookings",
+  },
+  {
+    id: "customers",
+    label: "Customers",
+    link: "/customers",
+  },
+  {
+    id: "categories",
+    label: "Categories",
+    link: "/categories",
+  },
+  
+
+];
+
+export { sidebarData };
