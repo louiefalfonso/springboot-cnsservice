802a17ee476a1a8586f58e08c1ef5917cfdfc591
Louie Alfonso <lfriasalfonso@gmail.com>
2024-11-18 16:02:08 +0000

    Update Amenities Client

--- client/src/components/amenities/Amenities.jsx
+++ client/src/components/amenities/Amenities.jsx
@@ -22,8 +22,25 @@ const Amenities = () => {
   
 
   return (
-    <div>Amenities</div>
-  )
+    <>
+      <div className="flex flex-col gap-4 min-h-[calc(100vh-212px)]">
+        <div className="grid grid-cols-1 gap-4">
+          <div className="p-5 bg-white border rounded border-black/10 dark:bg-darklight dark:border-darkborder">
+            <div className="flex items-center justify-between">
+              <h2 className="font-bold">Amenity List</h2>
+              <button
+                type="button"
+                onClick={toggleModal}
+                className="btn py-1 px-3.5 text-xs bg-warning border border-warning rounded-md text-black transition-all duration-300 hover:bg-warning/[0.85] hover:border-warning/[0.85]"
+              >
+                + Add New Amenity
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Amenities
