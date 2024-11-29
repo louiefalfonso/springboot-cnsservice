4e2d65aebc7f6669d3372783dde8d253e3858783
Louie Alfonso <lfriasalfonso@gmail.com>
2024-11-29 22:43:47 +0000

    Update Comments REST API

--- client/src/components/comments/UpdateComment.jsx
+++ client/src/components/comments/UpdateComment.jsx
@@ -1,6 +1,64 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import toast, { Toaster } from "react-hot-toast";
+import CommentService from "../../services/CommentService";
 
 const UpdateComment = () => {
+
+  const navigate = useNavigate();
+  const params = useParams();
+  const { id } = params;
+
+  const [commentId, setCommentId] = useState("");
+  const [bookingId, setBookingId] = useState("");
+  const [commentName, setCommentName] = useState("");
+  const [commentEmail, setCommentEmail] = useState("");
+  const [commentBody, setCommentBody] = useState("");
+
+  const [error, setError] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    const currentComment = {
+      commentName,
+      commentEmail,
+      commentBody,
+    };
+
+    CommentService.updateComment(id, currentComment)
+      .then((response) => {
+        navigate("/comments");
+      })
+      .catch((error) => {
+        setError(error);
+      });
+  };
+
+  useEffect(() => {
+    const fetchCurrentComment = async () => {
+      try {
+        const response = await CommentService.getCommentsById(id);
+        const update = response.data;
+        
+        setCommentName(update.commentName);
+        setCommentEmail(update.commentEmail);
+        setCommentBody(update.commentBody);
+
+      } catch (error) {
+        setError(error);
+      }
+    };  
+    fetchCurrentComment();
+  }, [id]);
+
+
   return (
     <div>UpdateComment</div>
   )
--- client/src/services/CommentService.js
+++ client/src/services/CommentService.js
@@ -3,15 +3,17 @@ import axios from "axios";
 const API_BASE_URL = import.meta.env.VITE_BASE_URI_BOOKINGS;
 
 const CommentService = {
-
   addNewComment(bookingId, commentData) {
     return axios.post(`${API_BASE_URL}/${bookingId}/comments`, commentData);
   },
 
   getAllComments(bookingId) {
     return axios.get(`${API_BASE_URL}/${bookingId}/comments`);
-  },  
+  },
 
+  getCommentById(commentId) {
+    return axios.get(`${API_BASE_URL}/comments/${commentId}`);
+  },
 
   getCommentsByBookingId(bookingId) {
     return axios.get(`${API_BASE_URL}/${bookingId}/comments`);
@@ -21,30 +23,6 @@ const CommentService = {
     return axios.delete(`${API_BASE_URL}/comments/${commentId}`);
   },
 
-  getCommentById(commentId) {
-    return axios.get(`${API_BASE_URL}/comments/${commentId}`);
-  },
-
-
-
-  /*
-
-   addNewComment(id) {
-    return axios.post(`${API_BASE_URL}/${id}/comments`, id);
-  },
-
-  getAllComments(id) {
-    return axios.get(`${API_BASE_URL}/${id}/comments`, id);
-  },
-
-  getCommentsByBookingId(id) {
-    return axios.get(`${API_BASE_URL}/${id}/comments`);
-  },
-
-  deleteComment(id) {
-    return axios.delete(`${API_BASE_URL}/${id}/comments`);
-  }
-    */
 };
 
 export default CommentService
